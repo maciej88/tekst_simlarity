1a68c71817394a03727c57155ed6303efa9ae1cf
maciej <mkrywult88@gmail.com>
2022-10-03 20:20:43 +0200

    Register class in app.py

--- web/app.py
+++ web/app.py
@@ -9,3 +9,29 @@ api = Api(app)
 client = MongoClient("mongo://db:27017")
 db = client.SimlarityDB
 users = db["Users"]
+
+class Register(Resource):
+    def post(self):
+        postedData = request.get_json()
+
+        username = postedData["username"]
+        password = postedData["password"]
+
+        if UserExist(username):
+            retJson = {
+                "status": 301,
+                "msg": "Invalid Username"
+            }
+            return jsonyfy(retJson)
+        hashed_pw = bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt())
+
+        users.insert({
+            "Username": username,
+            "Password": hashed_pw,
+            "Tokens": 6
+        })
+        retJson = {
+            "status": 200,
+            "msg": "Signed in!"
+        }
+        return jsonyfy(retJson)
