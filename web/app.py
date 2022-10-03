49bdf74739381599d95d72c7dfebadbfd0f1ef4c
maciej <mkrywult88@gmail.com>
2022-10-03 19:07:50 +0200

    app.py - first steps

---
+++ web/app.py
@@ -0,0 +1,9 @@
+from flask import Flask, jsonyfy, request
+from flask_restful import Api, Resource
+from pymongo import MongoClient
+import bcrypt
+
+app = Flask(__name__)
+api = Api(app)
+
+client = MongoClient("mongo://db:27017")
